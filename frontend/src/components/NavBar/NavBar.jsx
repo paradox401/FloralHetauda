67dda0e7910a3d5683cd2c32cf3fe7f7d6441b41
Sandesh Sapkota <sandeshsapkota@Sandeshs-MacBook-Air.local>
2025-02-04 15:14:44 +0545

    new

---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import 'boxicons'
+import './NavBar.css'
+
+function NavBar(props){
+    const [login, setLogin] = useState(false)
+    return(
+        <nav>
+            <img src="" alt="Floral Hetauda" />
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">FAQ</a>
+                </li>
+            </ul>
+            <div className="nav-icons">
+            <button id="cart"><i class='bx bx-cart'></i></button>
+            <button id = "login" > {login ? <i class='bx bx-user'></i> : "login" }</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
