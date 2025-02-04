1d08cf2731f7df62b294c69f82412c7b8c7bd8f3
Sandesh Sapkota <sandeshsapkota@Sandeshs-MacBook-Air.local>
2025-02-04 15:00:12 +0545

    navbar fix

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,23 +4,39 @@ import './NavBar.css'
 
 function NavBar(props){
     const [login, setLogin] = useState(false)
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return(
         <nav>
             <img src="" alt="Floral Hetauda" />
-            <ul>
-                <li>
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">FAQ</a>
-                </li>
-            </ul>
+            {isMobile ? (
+                <div className="hamburger-menu">
+                    <input type="checkbox" id="menu-toggle" />
+                    <label htmlFor="menu-toggle" className="menu-icon"><i class='bx bx-menu'></i></label>
+                    <ul className="menu">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">FAQ</a></li>
+                    </ul>
+                </div>
+            ) : (
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            )}
             <div className="nav-icons">
-            <button id="cart"><i class='bx bx-cart'></i></button>
-            <button id = "login" > {login ? <i class='bx bx-user'></i> : "login" }</button>
+                <button id="cart"><i class='bx bx-cart'></i></button>
+                <button id="login">{login ? <i class='bx bx-user'></i> : "login"}</button>
             </div>
         </nav>
     )
