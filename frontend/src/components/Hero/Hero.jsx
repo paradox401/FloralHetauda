9d939553063eb393097e421183f73ccbdffa2868
Paradox401 <sandeshsapkota371@gmail.com>
2025-02-05 14:53:04 +0545

    hero css

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import './Hero.css'
+import flowerImage from "./flowerImage.png"; 
+
 function Hero() {
     return (
         <div className="hero">
+            <div className="hero-img">
+                <img src={flowerImage} alt="flowers" />
+            </div>
+            <div className="hero-rectangle"></div>
+            <div className="hero-text">
+                <h1>Boquet Of Golden Hour Blooms</h1>
+            </div>
 
-        <div className="hero-rectangle">
-        </div>
-        <div className="hero-text">
-            <h1>Boquet Of Golden Hour Blooms</h1>
-        </div>
         </div>
     );
-    
 }
 
 export default Hero;
