1b9e8037adf643fd1e9bf57b42ae6f7825f0b476
Paradox401 <sandeshsapkota371@gmail.com>
2025-02-05 21:32:03 +0545

    Featured items

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,22 @@
+import flower1 from './flower1.png'
+import flower2 from './flower2.png'
+import flower3 from './flower3.png'
+import flower4 from './flower4.png'
+import flower5 from './flower5.png'
+
+const assets = {
+    flower1,
+    flower2,
+    flower3,
+    flower4,
+    flower5
+}
+
+export const flowers = [
+    { name: "Rose Bouquet", image: assets.flower1, price: "1399" },
+    { name: "Tulip Arrangement", image: assets.flower2, price: "1699" },
+    { name: "Lily Collection", image: assets.flower3, price: "1399"},
+    { name: "Rose Collection", image: assets.flower4, price: "1299"},
+    { name: "Paper Rose Bouquet", image : assets.flower5, price: "1799"}
+];
+
