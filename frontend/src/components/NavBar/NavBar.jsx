98a3cc5ddacbfc96a74770cbc360ba92b88a73a3
Paradox401 <sandeshsapkota371@gmail.com>
2025-02-09 21:00:56 +0545

    navbar fixes

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,31 +1,58 @@
-import React, { useEffect, useState } from "react";
-import 'boxicons'
-import './NavBar.css'
+import React, { useState } from "react";
+import 'boxicons';
+import './NavBar.css';
 import { assets } from "../../assets/assets";
 
-function NavBar(props){
-    const [login, setLogin] = useState(false)
-    return(
+function NavBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [login, setLogin] = useState(false);
+
+    // Function to toggle menu
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    // Function to close menu when clicking a link
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
+    return (
         <nav className="navbar">
-         <a href="#">  <img src= {assets.logo} alt="Floral Hetauda" /></a> 
-            <ul>
-                <li>
-                    <a href="#">HOME</a>
-                </li>
-                <li>
-                    <a href="#">ABOUT US</a>
-                </li>
-                <li>
-                    <a href="#">FAQ</a>
-                </li>
-            </ul>
-            <div className="nav-icons"> 
-            <button id="cart"><i class='bx bx-cart'></i></button>
-            <button id = "login" > {login ? <i class='bx bx-user'></i> : "Login" }</button>
+            {/* Logo */}
+            <a href="#">
+                <img src={assets.logo} alt="Floral Hetauda" />
+            </a>
+
+            {/* Hamburger Icon */}
+            <div className="hamburger" onClick={toggleMenu}>
+                <i className={menuOpen ? 'bx bx-x' : 'bx bx-menu'}></i>
             </div>
-            
+
+            {/* Menu */}
+            <ul className={menuOpen ? "active" : ""}>
+                <li><a href="#" onClick={closeMenu}>HOME</a></li>
+                <li><a href="#" onClick={closeMenu}>ABOUT US</a></li>
+                <li><a href="#" onClick={closeMenu}>FAQ</a></li>
+
+                {/* Nav Icons Inside Menu for Mobile */}
+                <div className="mobile-nav-icons">
+                    <button id="cart" onClick={closeMenu}>
+                        <i className='bx bx-cart'></i>
+                    </button>
+                    <button id="login" onClick={closeMenu}>
+                        {login ? <i className='bx bx-user'></i> : "Login"}
+                    </button>
+                </div>
+            </ul>
+
+            {/* Desktop Nav Icons */}
+            <div className="nav-icons">
+                <button id="cart"><i className='bx bx-cart'></i></button>
+                <button id="login">{login ? <i className='bx bx-user'></i> : "Login"}</button>
+            </div>
         </nav>
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;
